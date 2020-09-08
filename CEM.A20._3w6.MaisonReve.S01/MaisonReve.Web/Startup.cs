8ce325b57372d6aab42d0aaf2bbeeff110a0eac9
Tony <antoine@ticonseil.com>
2020-09-08 11:08:59 -0400

    Ajout de commentaire sur le starup.cs

--- CEM.A20._3w6.MaisonReve.S01/MaisonReve.Web/Startup.cs
+++ CEM.A20._3w6.MaisonReve.S01/MaisonReve.Web/Startup.cs
@@ -23,6 +23,7 @@ namespace MaisonReve.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Ajouter les vues et controlleurs au DI
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
             services.AddSingleton<BuildingRepo>();
