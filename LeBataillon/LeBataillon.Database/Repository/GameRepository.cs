a9e61c49da459b8dd8d804cefef2cc558435086b
Tony <antoine@ticonseil.com>
2020-10-08 14:01:03 -0400

    Implantation des models

---
+++ LeBataillon/LeBataillon.Database/Repository/GameRepository.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeBataillon.Database.Context;
+using LeBataillon.Database.Exceptions;
+using LeBataillon.Database.Models;
+
+namespace LeBataillon.Database.Repository
+{
+    public class GameRepository
+    {
+        private readonly LeBataillonDbContext _context;
+        public GameRepository(LeBataillonDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Game> GetAll()
+        {
+            return _context.Games.ToList();
+        }
+
+        public Game GetById(int id)
+        {
+            return _context.Games
+               
+                .FirstOrDefault(m => m.GameId == id);
+        }
+        public void Add(Game game)
+        {
+            _context.Add(game);
+            _context.SaveChanges();
+        }
+
+        public void Edit(Game game)
+        {
+            _context.Update(game);
+            _context.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            var game = _context.Games.Find(id);
+            _context.Games.Remove(game);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ LeBataillon/LeBataillon.Database/Repository/TeamRepository.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeBataillon.Database.Context;
+using LeBataillon.Database.Exceptions;
+using LeBataillon.Database.Models;
+
+namespace LeBataillon.Database.Repository
+{
+    public class TeamRepository
+    {
+        private readonly LeBataillonDbContext _context;
+        public TeamRepository(LeBataillonDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Team> GetAll()
+        {
+            return _context.Teams.ToList();
+        }
+
+        public Team GetById(int id)
+        {
+            return _context.Teams
+               
+                .FirstOrDefault(m => m.TeamId == id);
+        }
+        public void Add(Team Team)
+        {
+            _context.Add(Team);
+            _context.SaveChanges();
+        }        
+        public void Edit(Team Team)
+        {
+            _context.Update(Team);
+            _context.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            var Team = _context.Teams.Find(id);
+            _context.Teams.Remove(Team);
+            _context.SaveChanges();
+        }
+    }
+}
